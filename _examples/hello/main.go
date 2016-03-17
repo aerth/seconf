1d14711416274714de9832c9cf8fbb51cad31433
aerth <aerth@isupon.us>
2016-03-17 14:59:34 -0700

    add license and example

--- _examples/hello/main.go
+++ _examples/hello/main.go
@@ -1,46 +1,49 @@
 package main
 
 import (
-"github.com/aerth/seconf"
-"os"
-"fmt"
-"strings"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aerth/seconf"
 )
+
 func main() {
-  if len(os.Args) < 3 {
-      fmt.Println("Usage:")
-      fmt.Println(os.Args[0]+" configname servicename field1 field2 etc")
-      fmt.Println(os.Args[0]+" Tester MyApplication username favorite-color password favorite-celebrity")
-      os.Exit(1)
-  }
+	if len(os.Args) < 3 {
+		fmt.Println("Usage:")
+		fmt.Println(os.Args[0] + " configname servicename field1 field2 etc")
+		fmt.Println("Example:")
+		fmt.Println(os.Args[0] + " SuperConfig FirstSeconf username favorite-color password favorite-celebrity")
+		os.Exit(1)
+	}
 
-s := os.Args[1]
-sn := os.Args[2]
-var fields []string
-fields = os.Args[3:]
+	s := os.Args[1]
+	sn := os.Args[2]
+	var fields []string
+	fields = os.Args[3:]
 
-if !seconf.Detect(s) {
-  seconf.Create(s, sn, fields...)
-  }else{
-    configdecoded, err := seconf.Read(s)
-    if err != nil {
-      fmt.Println("error:")
-      fmt.Println(err)
-      os.Exit(1)
-    }
+	if !seconf.Detect(s) {
+		seconf.Create(s, sn, fields...)
+	} else {
+		configdecoded, err := seconf.Read(s)
+		if err != nil {
+			fmt.Println("error:")
+			fmt.Println(err)
+			os.Exit(1)
+		}
 		configarray := strings.Split(configdecoded, "::::")
-    if len(configarray) < 2 {
-      fmt.Println("Broken config file. Create a new one.")
-      os.Exit(1)
-    }
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
-    }
-    fmt.Println("Welcome to "+sn+", "+configarray[0])
-    fmt.Printf("Your %s is %s \n", os.Args[3], configarray[0])
-    fmt.Printf("Your %s is %s \n", os.Args[4], configarray[1])
-    fmt.Printf("Your %s is %s \n", os.Args[5], configarray[2])
+		if len(configarray) < 2 {
+			fmt.Println("Broken config file. Create a new one.")
+			os.Exit(1)
+		}
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println("Welcome to " + sn + ", " + configarray[0])
+		fmt.Printf("Your %s is %s \n", os.Args[3], configarray[0])
+		fmt.Printf("Your %s is %s \n", os.Args[4], configarray[1])
+		fmt.Printf("Your %s is %s \n", os.Args[5], configarray[2])
 
-  }
+	}
 }
--- seconf.go
+++ seconf.go
@@ -1,5 +1,85 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 aerth
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// package seconf allows your software to store non-plaintext configuration files.
 package seconf
 
+// Here is an example application that stores and retrieves four fields.
+// The third field in this example begins with "pass" so is interpreted as a password and will not be echoed.
+//
+// ```
+//
+// package main
+//
+// import (
+// 	"fmt"
+// 	"os"
+// 	"strings"
+//
+// 	"github.com/aerth/seconf"
+// )
+//
+// func main() {
+// 	if len(os.Args) < 3 {
+// 		fmt.Println("Usage:")
+// 		fmt.Println(os.Args[0] + " configname servicename field1 field2 etc")
+// 		fmt.Println("Example:")
+// 		fmt.Println(os.Args[0] + " SuperConfig FirstSeconf username favorite-color password favorite-celebrity")
+// 		os.Exit(1)
+// 	}
+//
+// 	s := os.Args[1]
+// 	sn := os.Args[2]
+// 	var fields []string
+// 	fields = os.Args[3:]
+//
+// 	if !seconf.Detect(s) {
+// 		seconf.Create(s, sn, fields...)
+// 	} else {
+// 		configdecoded, err := seconf.Read(s)
+// 		if err != nil {
+// 			fmt.Println("error:")
+// 			fmt.Println(err)
+// 			os.Exit(1)
+// 		}
+// 		configarray := strings.Split(configdecoded, "::::")
+// 		if len(configarray) < 2 {
+// 			fmt.Println("Broken config file. Create a new one.")
+// 			os.Exit(1)
+// 		}
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			os.Exit(1)
+// 		}
+// 		fmt.Println("Welcome to " + sn + ", " + configarray[0])
+// 		fmt.Printf("Your %s is %s \n", os.Args[3], configarray[0])
+// 		fmt.Printf("Your %s is %s \n", os.Args[4], configarray[1])
+// 		fmt.Printf("Your %s is %s \n", os.Args[5], configarray[2])
+//
+// 	}
+// }
+//
+// ```
+//
 import (
 	"bufio"
 	"crypto/rand"
