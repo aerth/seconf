f5c638d19efcecd2172168534da2e42927c4ad83
aerth <aerth@isupon.us>
2016-03-17 15:03:21 -0700

    godoc?

--- seconf.go
+++ seconf.go
@@ -21,64 +21,11 @@
 // SOFTWARE.
 
 // package seconf allows your software to store non-plaintext configuration files.
-// Here is an example application that stores and retrieves four fields.
-// The third field in this example begins with "pass" so is interpreted as a password and will not be echoed.
-//
-// ```
-//
-// package main
-//
-// import (
-// 	"fmt"
-// 	"os"
-// 	"strings"
-//
-// 	"github.com/aerth/seconf"
-// )
-//
-// func main() {
-// 	if len(os.Args) < 3 {
-// 		fmt.Println("Usage:")
-// 		fmt.Println(os.Args[0] + " configname servicename field1 field2 etc")
-// 		fmt.Println("Example:")
-// 		fmt.Println(os.Args[0] + " SuperConfig FirstSeconf username favorite-color password favorite-celebrity")
-// 		os.Exit(1)
-// 	}
-//
-// 	s := os.Args[1]
-// 	sn := os.Args[2]
-// 	var fields []string
-// 	fields = os.Args[3:]
-//
-// 	if !seconf.Detect(s) {
-// 		seconf.Create(s, sn, fields...)
-// 	} else {
-// 		configdecoded, err := seconf.Read(s)
-// 		if err != nil {
-// 			fmt.Println("error:")
-// 			fmt.Println(err)
-// 			os.Exit(1)
-// 		}
-// 		configarray := strings.Split(configdecoded, "::::")
-// 		if len(configarray) < 2 {
-// 			fmt.Println("Broken config file. Create a new one.")
-// 			os.Exit(1)
-// 		}
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			os.Exit(1)
-// 		}
-// 		fmt.Println("Welcome to " + sn + ", " + configarray[0])
-// 		fmt.Printf("Your %s is %s \n", os.Args[3], configarray[0])
-// 		fmt.Printf("Your %s is %s \n", os.Args[4], configarray[1])
-// 		fmt.Printf("Your %s is %s \n", os.Args[5], configarray[2])
-//
-// 	}
-// }
-//
-// ```
-//
-
+// check out the example application in _examples/hello for a working example.
+// fmt.Println("Welcome to " + sn + ", " + configarray[0])
+// fmt.Printf("Your %s is %s \n", os.Args[3], configarray[0])
+// fmt.Printf("Your %s is %s \n", os.Args[4], configarray[1])
+// fmt.Printf("Your %s is %s \n", os.Args[5], configarray[2])
 package seconf
 
 import (
